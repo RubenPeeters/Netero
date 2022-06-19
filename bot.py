346d2d6d82b196480bb2c644a1450386a58099f0
RubenPeeters <40251899+RubenPeeters@users.noreply.github.com>
2022-06-19 23:09:04 +0200

    default implementation based on r danny

--- bot.py
+++ bot.py
@@ -1,22 +1,96 @@
 import discord
+from discord.ext import commands
+import traceback
+import logging
+import config
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+log = logging.getLogger(__name__)
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+class Netero(commands.Bot):
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    @property
+    def owner(self) -> discord.User:
+        return self.bot_app_info.owner
 
-client.run('')
+    @property
+    def config(self):
+        return __import__('config')
+
+    def __init__(self):
+        intents = discord.Intents(
+            guilds=True,
+            members=True,
+            bans=True,
+            emojis=True,
+            voice_states=True,
+            messages=True,
+            reactions=True,
+            message_content=True,
+        )
+        super().__init__(
+            command_prefix='!',
+            description='Ruben Peeters personal discord bot',
+            heartbeat_timeout=150.0,
+            intents=intents,
+            enable_debug_events=True,
+        )
+
+        self.client_id: str = config.client_id
+
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send('This command cannot be used in private messages.')
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.author.send('Sorry. This command is disabled and cannot be used.')
+        elif isinstance(error, commands.CommandInvokeError):
+            original = error.original
+            if not isinstance(original, discord.HTTPException):
+                print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
+                traceback.print_tb(original.__traceback__)
+                print(f'{original.__class__.__name__}: {original}',
+                      file=sys.stderr)
+        elif isinstance(error, commands.ArgumentParsingError):
+            await ctx.send(str(error))
+
+    async def on_ready(self):
+        if not hasattr(self, 'uptime'):
+            self.uptime = discord.utils.utcnow()
+
+        print(f'Ready: {self.user} (ID: {self.user.id})')
+
+    async def process_commands(self, message: discord.Message):
+        ctx = await self.get_context(message)
+
+        if ctx.command is None:
+            return
+
+        if ctx.author.id in self.blacklist:
+            return
+
+        if ctx.guild is not None and ctx.guild.id in self.blacklist:
+            return
+
+        try:
+            await self.invoke(ctx)
+        finally:
+            pass
+
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.bot:
+            return
+        await self.process_commands(message)
+
+    async def on_guild_join(self, guild: discord.Guild) -> None:
+        if guild.id in self.blacklist:
+            await guild.leave()
+
+    async def start(self) -> None:
+        await super().start(config.token, reconnect=True)
+
+    async def close(self) -> None:
+        await super().close()
+        await self.session.close()
+
+    async def start(self) -> None:
+        await super().start(config.token, reconnect=True)
---
+++ start.py
@@ -0,0 +1,71 @@
+from bot import Netero
+import contextlib
+import logging
+import click
+import asyncio
+
+from logging.handlers import RotatingFileHandler
+
+
+@contextlib.contextmanager
+def setup_logging():
+    log = logging.getLogger()
+
+    try:
+        # __enter__
+        max_bytes = 32 * 1024 * 1024  # 32 MiB
+        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord.http').setLevel(logging.WARNING)
+        logging.getLogger('discord.state').addFilter(RemoveNoise())
+
+        log.setLevel(logging.INFO)
+        handler = RotatingFileHandler(
+            filename='netero.log', encoding='utf-8', mode='w', maxBytes=max_bytes, backupCount=5)
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
+        fmt = logging.Formatter(
+            '[{asctime}] [{levelname:<7}] {name}: {message}', dt_fmt, style='{')
+        handler.setFormatter(fmt)
+        log.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        handlers = log.handlers[:]
+        for hdlr in handlers:
+            hdlr.close()
+            log.removeHandler(hdlr)
+
+
+class RemoveNoise(logging.Filter):
+    def __init__(self):
+        super().__init__(name='discord.state')
+
+    def filter(self, record):
+        if record.levelname == 'WARNING' and 'referencing an unknown' in record.msg:
+            return False
+        return True
+
+
+async def start():
+    log = logging.getLogger()
+    kwargs = {
+        'command_timeout': 60,
+        'max_size': 20,
+        'min_size': 20,
+    }
+
+    bot = Netero()
+    await bot.start()
+
+
+@click.group(invoke_without_command=True, options_metavar='[options]')
+@click.pass_context
+def default_start(ctx):
+    """Launches the bot."""
+    if ctx.invoked_subcommand is None:
+        with setup_logging():
+            asyncio.run(start())
+
+
+if __name__ == '__main__':
+    default_start()

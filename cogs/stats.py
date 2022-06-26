44bc58c29830a23024a19865cf108b0cafc037b0
RubenPeeters <40251899+RubenPeeters@users.noreply.github.com>
2022-06-26 12:47:51 +0200

    Small look update stats

--- cogs/stats.py
+++ cogs/stats.py
@@ -57,8 +57,8 @@ class Stats(commands.Cog):
             text = len(text_channels)
             voice = len(voice_channels)
             payload = f' \
-            ```{"Members":8}: {total_members:16d}```\n \
-            ```{"Channels":8}: {text + voice:16d}```\n \
+            ```{"Members":8}: {total_members:16}```\n \
+            ```{"Channels":8}: {text + voice:16}```\n \
             ```{"Servers":8}: {str(len(self.bot.guilds)):16}```\n \
             ```{"Uptime":8}: {self.get_bot_uptime(brief=True):16}```\n \
             '

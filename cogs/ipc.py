bb03e74d9afeacdf6a07cbb3f2888040b6a9f529
RubenPeeters <40251899+RubenPeeters@users.noreply.github.com>
2022-07-03 18:38:25 +0200

    epic site making music continues

--- cogs/ipc.py
+++ cogs/ipc.py
@@ -16,6 +16,20 @@ class IpcRoutes(commands.Cog):
             final.append(guild.id)
         return final  # returns the guild ids to the client
 
+    @ipc.server.route()
+    async def get_guild(self, data):
+        guild = self.get_guild(data.guild_id)
+        if guild is None:
+            return None
+
+        guild_data = {
+            "name": guild.name,
+            "id": guild.id,
+            "prefix": "?"
+        }
+
+        return guild_data
+
 
 async def setup(bot):
     await bot.add_cog(IpcRoutes(bot))
--- web.py
+++ web.py
@@ -18,7 +18,7 @@ discord = DiscordOAuth2Session(app)
 
 @app.route("/")
 async def home():
-    return await render_template("index.html")
+    return await render_template("index.html", authorized=await discord.authorized)
 
 
 @app.route("/login")
@@ -30,31 +30,44 @@ async def login():
 async def callback():
     try:
         await discord.callback()
-    except:
-        return redirect(url_for("login"))
+    except Exception:
+        pass
 
-    user = await discord.fetch_user()
-    # You should return redirect(url_for("dashboard")) here
-    return f"{user.name}#{user.discriminator}"
+    return redirect(url_for("dashboard"))
 
 
 @app.route("/dashboard")
 async def dashboard():
+    if not await discord.authorized:
+        return redirect(url_for("login"))
+
     guild_count = await ipc_client.request("get_guild_count")
     guild_ids = await ipc_client.request("get_guild_ids")
 
-    try:
-        user_guilds = await discord.fetch_guilds()
-    except:
-        return redirect(url_for("login"))
+    user_guilds = await discord.fetch_guilds()
 
-    same_guilds = []
+    guilds = []
 
     for guild in user_guilds:
-        if guild.id in guild_ids:
-            same_guilds.append(guild)
+        if guild.permissions.administrator:
+            guild.class_color = "green-border" if guild.id in guild_ids else "red-border"
+            guilds.append(guild)
 
-    return await render_template("dashboard.html", guild_count=guild_count, matching=same_guilds)
+    guilds.sort(key=lambda x: x.class_color == "red-border")
+    name = (await discord.fetch_user()).name
+    return await render_template("dashboard.html", guild_count=guild_count, guilds=guilds, username=name)
+
+
+@app.route("/dashboard/<int:guild_id>")
+async def dashboard_server(guild_id):
+    if not await discord.authorized:
+        return redirect(url_for("login"))
+
+    guild = await ipc_client.request("get_guild", guild_id=guild_id)
+    if guild is None:
+        return redirect(f'https://discord.com/oauth2/authorize?&client_id={app.config["DISCORD_CLIENT_ID"]}&scope=bot&permissions=8&guild_id={guild_id}&response_type=code&redirect_uri={app.config["DISCORD_REDIRECT_URI"]}')
+    return guild["name"]
+
 
 if __name__ == "__main__":
     app.run(debug=True)

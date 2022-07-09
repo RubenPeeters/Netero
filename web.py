9cfcb177e104429219c9d42b7e370eaed60a76d4
RubenPeeters <40251899+RubenPeeters@users.noreply.github.com>
2022-07-09 23:19:42 +0200

    Update web.py

--- web.py
+++ web.py
@@ -76,7 +76,7 @@ async def dashboard_server(guild_id):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop() or asyncio.new_event_loop()
     serve_config = Config()
-    serve_config.bind = [f"{config.host}:80"]
+    serve_config.bind = [f"{config.public_ip}:80"]
     print(ipc_client)
     try:
         # `Client.start()` returns new Client instance or None if it fails to start
